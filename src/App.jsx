d1ecf63b43dc5c781d3a6992ed937b4ec58821ec
Pranav Rajput <pranavrajput182002@gmail.com>
2023-11-16 19:48:01 +0530

    eslint-import config

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 
 import './App.css'
 
+import {  Routes } from 'react-router-dom'
+
 function App() {
  
 
 
   return (
     <>
-    Pbl
+    <Routes>
+
+    </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+//component import
+//css import
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//library imports
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 
-    <App />
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+   
   
 )
